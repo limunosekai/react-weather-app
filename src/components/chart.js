e9d0af0b496a8f87b86907f7af17d7ae6a5c7558
limunosekai <powerlsh0103@gmail.com>
2021-05-12 15:17:30 +0900

    Add chart's label

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine,
+} from 'react-sparklines';
 
 function chart(props) {
+  const average = (data) => {
+    return _.round(_.sum(data) / data.length);
+  };
+
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type='avg' />
       </Sparklines>
+      <div>
+        {average(props.data)} {props.units}
+      </div>
     </div>
   );
 }
--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Chart from '../components/chart';
+import _ from 'lodash';
 
 function weatherList() {
   const weather = useSelector((state) => state.weather);
 
   const renderWeather = (cityData) => {
     const name = cityData.city.name;
-    const temps = cityData.list.map((weather) => weather.main.temp);
-    // const humidities = cityData.list.map((weather) => weather.main.humidity);
+    const temps = _.map(
+      cityData.list.map((weather) => weather.main.temp),
+      (temp) => temp - 273
+    );
+    const humidities = cityData.list.map((weather) => weather.main.humidity);
+    const pressueres = cityData.list.map((weather) => weather.main.pressure);
 
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color='blue' />
+          <Chart data={temps} color='blue' units='℃' />
+        </td>
+        <td>
+          <Chart data={pressueres} color='green' units='hPa' />
+        </td>
+        <td>
+          <Chart data={humidities} color='red' units='%' />
         </td>
       </tr>
     );
@@ -25,9 +36,9 @@ function weatherList() {
       <thead>
         <tr>
           <th>City</th>
-          <th>Temperature</th>
-          <th>Pressure</th>
-          <th>Humidity</th>
+          <th>Temperature (℃)</th>
+          <th>Pressure (hPa)</th>
+          <th>Humidity (%)</th>
         </tr>
       </thead>
       <tbody>{weather.map(renderWeather)}</tbody>
